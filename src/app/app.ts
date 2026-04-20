4459d73f02a4dcbbefda0609b2eff8d92528ff7b
DanielRiveroP <riveroperezmesa@gmail.com>
2026-04-20 13:07:30 +0100

    Complete Smart Group landing page layout and user dropdown

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,46 @@
-import { Component, signal } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { CommonModule } from '@angular/common'; // Importante para usar *ngFor
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('smartgrouptnf');
+export class AppComponent implements AfterViewInit {
+  
+  // Listado de propiedades (Actualizado con mejores imágenes)
+  viviendas = [
+    {
+      nombre: 'Villa Luxury Vista Mar Adeje',
+      ciudad: 'Costa Adeje',
+      habitaciones: 5,
+      precio: 150,
+      imagen: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=600' // <--- FOTO ARREGLADA
+    },
+    {
+      nombre: 'Ático Moderno Centro Santa Cruz',
+      ciudad: 'Santa Cruz',
+      habitaciones: 2,
+      precio: 75,
+      imagen: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=600'
+    },
+    {
+      nombre: 'Casa Rural con Encanto en Vilaflor',
+      ciudad: 'Vilaflor',
+      habitaciones: 3,
+      precio: 110,
+      imagen: 'https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?q=80&w=600'
+    }
+  ];
+
+  ngAfterViewInit(): void {
+    // Esto es lo que activa el menú al hacer clic
+    setTimeout(() => {
+      initFlowbite();
+    }, 1);
+  }
 }
