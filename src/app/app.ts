d2c54a7be338dc51a20cab777eb3cd71cfca11b9
DanielRiveroP <riveroperezmesa@gmail.com>
2026-04-21 10:51:25 +0100

    añadidas traduciones

--- src/app/app.ts
+++ src/app/app.ts
@@ -12,33 +12,34 @@ import { CommonModule } from '@angular/common'; // Importante para usar *ngFor
 })
 export class AppComponent implements AfterViewInit {
   
-  // Listado de propiedades (Actualizado con mejores imágenes)
+  
+  companyName = 'Olax';
+
+  
   viviendas = [
     {
-      nombre: 'Villa Luxury Vista Mar Adeje',
+      nombre: 'Villa Horizon Adeje',
       ciudad: 'Costa Adeje',
       habitaciones: 5,
+      huespedes: 10, 
+      camas: 6,      
+      banos: 3,      
       precio: 150,
-      imagen: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=600' // <--- FOTO ARREGLADA
+      imagen: 'https://images.unsplash.com/photo-1600596542815-ffad4c1539a9?q=80&w=600'
     },
     {
-      nombre: 'Ático Moderno Centro Santa Cruz',
+      nombre: 'Ático Centro Santa Cruz',
       ciudad: 'Santa Cruz',
       habitaciones: 2,
+      huespedes: 4,  
+      camas: 2,      
+      banos: 1,      
       precio: 75,
       imagen: 'https://images.unsplash.com/photo-1512917774080-9991f1c4c750?q=80&w=600'
-    },
-    {
-      nombre: 'Casa Rural con Encanto en Vilaflor',
-      ciudad: 'Vilaflor',
-      habitaciones: 3,
-      precio: 110,
-      imagen: 'https://images.unsplash.com/photo-1583608205776-bfd35f0d9f83?q=80&w=600'
     }
   ];
 
   ngAfterViewInit(): void {
-    // Esto es lo que activa el menú al hacer clic
     setTimeout(() => {
       initFlowbite();
     }, 1);
